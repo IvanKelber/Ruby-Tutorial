836d702b8c75dfb4c501c4cf3d58cac069d7b664
Ivan Kelber <ivankelber@gmail.com>
2017-02-09 13:06:48 -0500

    added signin tests as well as errors on invlid user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#signup'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
+
 
   resources :users
 
