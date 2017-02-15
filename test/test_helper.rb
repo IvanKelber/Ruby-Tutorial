50dbfa1a8c78c996616a1b9d05c74187408c2212
Ivan Kelber <ivankelber@gmail.com>
2017-02-15 00:16:37 -0500

    added helper method for logging in as a particular user for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,10 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !!session[:user_id]
   end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
 end
