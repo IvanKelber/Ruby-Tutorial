3c0c649b6fc38164b8c218cf151a8fcf7a78d219
Ivan Kelber <ivankelber@gmail.com>
2017-02-14 22:56:57 -0500

    implemented working log in and log out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      # flash[:success] = "Login successful"
+      log_in user
+      redirect_to user_url(user)
+    else
+      flash.now[:danger] = "Invalid email/password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+end
