482d7b812a274c2f9dd6329aae4b8f5ccfefa57c
Ivan Kelber <ivankelber@gmail.com>
2017-02-15 19:59:09 -0500

    added user index

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :users
+  resources :users #this handles a bunch of things for us!!
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @glick = users(:glick)
+    @cj = users(:cj)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 
 end
