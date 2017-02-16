24da385b1bc55680b5141661cd0e573311e4c02a
Ivan Kelber <ivankelber@gmail.com>
2017-02-16 16:12:09 -0500

    changed host name to include https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   #email settings
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'blooming-stream-48852.herokuapp.com'
+  host = 'https://blooming-stream-48852.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
