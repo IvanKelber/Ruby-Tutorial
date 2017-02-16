5faf2d6305685a76cae19094bde3499fe99f48dd
Ivan Kelber <ivankelber@gmail.com>
2017-02-15 20:29:56 -0500

    Finished destroying users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index,:edit, :update]
+  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
   before_action :correct_user, only: [:edit,:update]
+  before_action :admin_user,     only: :destroy
+
 
   def index
     @users = User.paginate(page: params[:page],per_page:10)
@@ -23,6 +25,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = "User: #{@user.name} has been deleted!"
+    redirect_to users_path
   end
 
   def edit
@@ -64,6 +72,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user? @user
+    end
+
+    def admin_user
+      redirect_to root_path unless current_user.admin?
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "a@a.a",
+User.create!(name:  "Ivan Kelber",
+             email: "ivankelber@gmail.com",
              password:              "password",
-             password_confirmation: "password")
+             password_confirmation: "password",
+             admin: true)
 
 49.times do |n|
   name  = Faker::Name.name
