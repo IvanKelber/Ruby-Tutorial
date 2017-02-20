3d40201c9b08d6a78c4d9a240b87cc9a6096e199
Ivan Kelber <ivankelber@gmail.com>
2017-02-20 16:01:39 -0500

    users can destroy their own posts now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user? @user
-  end
-
   def admin_user
     redirect_to root_path unless current_user.admin?
   end
-  
+
 end
